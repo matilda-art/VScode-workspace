b8b6cf39ed3cf29fdead39d95c1d7de5c29268aa
matilda-art <heysober@163.com>
2020-08-20 20:53:46 +0800

    二分查找

---
+++ 二分查找/BinarySearch.java
@@ -0,0 +1,25 @@
+//二分查找
+
+public class BinarySearch {
+    public static void main(String[] args) {
+        int[] arr = {4,9,2,6,7,11};
+
+    }
+
+    public static void binarySearch(int[] arr, int toFind){
+        int left = 0;
+        int right = arr.length-1;
+
+        while(left <= right){
+            int mid = (left + right) / 2;
+            if(toFind < arr[mid]){
+                right = mid - 1;
+            }else if(toFind > arr[mid]){
+                left = mid + 1;
+            }else {
+                return mid;
+            }
+        }
+        return -1;
+    }
+}
