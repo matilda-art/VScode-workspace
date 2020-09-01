73943130f03af9d1d6d1528508272bd78d7e43c5
matilda-art <heysober@163.com>
2020-09-01 22:55:04 +0800

    直接插入排序

---
+++ 直接插入排序/InsertSort.java
@@ -0,0 +1,27 @@
+//直接插入排序
+
+public class InsertSort{
+
+    public static void main(String[] args){
+        int[] arr = {12,33,42,1,7,10};
+        insertSort(arr);
+        System.out.println(Arrays.tostring(arr));
+    }
+
+    public static void insertSort(int[] arr){
+        //从第二项开始
+        for(int i = 1;i < arr.length;i++){
+            int tmp = arr[i];
+            int j = i-1；
+            for( ;j >= 0;j--){
+                if(arr[j] > tmp){
+                    arr[j+1] = arr[j];
+                }else{
+                    //arr[j+1] = tmp;
+                    break;
+                }
+            }
+            arr[j+1] = tmp;
+        }
+    }
+}
