c7c9810f3a9780fcff5f88ca66f4e9df1a74e4fb
小楫轻舟 <62049028+matilda-art@users.noreply.github.com>
2020-09-20 22:01:27 +0800

    替换空格

--- 替换空格/Solution.java
+++ 替换空格/Solution.java
@@ -10,10 +10,11 @@ class Solution{
         StringBuilder sb = new StringBuilder();
         for(int i = 0;i < s.length();i++){
             char c = s.charAt(i);
-            if(c == ' ')
+            if(c == ' '){
                 sb.append("%20");
-            else
+            }else{
                 sb.append(c);
+            }
         }
         return sb.toString();
     }
