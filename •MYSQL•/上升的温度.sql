ab01261255fbc3b930d0494cf601a9c980754762
小楫轻舟 <62049028+matilda-art@users.noreply.github.com>
2020-09-23 22:40:58 +0800

    sql练习

--- •MYSQL•/上升的温度.sql
+++ •MYSQL•/上升的温度.sql
@@ -1,4 +1,4 @@
 -- 给定一个 Weather 表，编写一个 SQL 查询，来查找与之前（昨天的）日期相比温度更高的所有日期的 Id。
 select a.id from weather a join weather b 
 on a.temperature > b.temperature and datediff(a.recorddate,b.recorddate) = 1
--- datediff是datea-dateb的天数差
+-- datediff是datea - dateb的天数差
